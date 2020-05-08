bbe15ee4712d44a665df3edb16f79bd76d6a3590
animesh <royanimesh2211@gmail.com>
2020-05-08 20:08:23 +0530

    (TDD) Testing hitpoint

---
+++ app/src/main/java/com/raywenderlich/android/creaturemon/model/CreatureGenerator.kt
@@ -0,0 +1,14 @@
+package com.raywenderlich.android.creaturemon.model
+
+class CreatureGenerator {
+
+    fun generateCreature(attributes: CreatureAttributes, name: String = "",
+    drawable: Int = 0) : Creature {
+
+        val hitPoint = 5 * attributes.intelligence +
+                3 * attributes.strength +
+                4 * attributes.endurance
+
+        return Creature(attributes, hitPoint, name)
+    }
+}
