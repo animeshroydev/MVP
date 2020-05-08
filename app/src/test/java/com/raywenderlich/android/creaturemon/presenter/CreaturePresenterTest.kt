30909116770cea98b9fd84390fe12e0068713fbe
animesh <royanimesh2211@gmail.com>
2020-05-08 23:32:21 +0530

    Test for STRENGTH and ENDURANCE

--- app/src/test/java/com/raywenderlich/android/creaturemon/presenter/CreaturePresenterTest.kt
+++ app/src/test/java/com/raywenderlich/android/creaturemon/presenter/CreaturePresenterTest.kt
@@ -37,4 +37,24 @@ class CreaturePresenterTest {
 
         verify(view, times(1)).showHitPoints("50")
     }
+
+    @Test
+    fun testStrengthSelected() {
+        val attributes = CreatureAttributes(0,3,0)
+        val stubCreature = Creature(attributes, 9)
+        `when`(mockGenerator.generateCreature(attributes)).thenReturn(stubCreature)
+
+        presenter.attributeSelected(AttributeType.STRENGTH, 1)
+        verify(view, times(1)).showHitPoints("9")
+    }
+
+    @Test
+    fun tstEnduranceSelected() {
+        val attributes = CreatureAttributes(0,0,7)
+        val stubCreature = Creature(attributes, 28)
+        `when`(mockGenerator.generateCreature(attributes)).thenReturn(stubCreature)
+
+        presenter.attributeSelected(AttributeType.ENDURANCE,2)
+        verify(view, times(1)).showHitPoints("28")
+    }
 }
